57d581c7331be279a5616d57f61ce1fd82b83391
spetruzza <steve.petruzza@gmail.com>
2017-05-25 14:07:30 -0600

    visit_idx_utils contains general utility functions

--- src/databases/IDX/uintah_utils.h
+++ src/databases/IDX/uintah_utils.h
@@ -7,6 +7,7 @@
 #define UINTAH_IDX_UTILS_H
 
 #include "LevelInfo.h"
+#include "visit_idx_utils.h"
 #include <vtkSmartPointer.h>
 #include <vtkXMLDataParser.h>
 #include <sstream>
@@ -15,25 +16,5 @@ extern void ups_parse_vector(vtkXMLDataElement *el, int* vec, int dim);
 extern void ups_parse_vector(vtkXMLDataElement *el, double* vec, int dim);
 
 extern void parse_ups(vtkSmartPointer<vtkXMLDataParser> parser, LevelInfo& levelInfo, int dim, bool use_extracells);
-
-static inline int        cint   (std::string s) {int    value;std::istringstream iss(s);iss>>value;return value;}
-static inline float      cfloat (std::string s) {float  value;std::istringstream iss(s);iss>>value;return value;}
-static inline double     cdouble(std::string s) {double value;std::istringstream iss(s);iss>>value;return value;}
-// trim from start
-static inline std::string &ltrim(std::string &s) {
-  s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
-  return s;
-}
-
-// trim from end
-static inline std::string &rtrim(std::string &s) {
-  s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
-  return s;
-}
-
-// trim from both ends
-static inline std::string &trim(std::string &s) {
-  return ltrim(rtrim(s));
-}
 
 #endif //UINTAH_IDX_UTILS_H
